597278e1136764577761111b1778b146c95e4170
Jim Brighter <jim-brighter@users.noreply.github.com>
2019-07-21 00:55:06 -0400

    Update 2-setup.sh

--- linux/arch-setup/2-setup.sh
+++ linux/arch-setup/2-setup.sh
@@ -36,21 +36,3 @@ mkdir -p ~/.config/rofi
 cp .config/rofi/theme.rasi ~/.config/rofi/theme.rasi
 
 sudo cp etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-
-cd ~
-curl -LO https://github.com/vinceliuice/matcha/archive/master.zip
-unzip -q master.zip
-rm master.zip
-cd matcha-master
-chmod +x Install
-./Install
-
-cd ~
-curl -LO https://github.com/KaizIqbal/Bibata_Cursor/archive/master.zip
-unzip -q master.zip
-rm master.zip
-cd Bibata_Cursor-master
-chmod +x build.sh
-./build.sh
-chmod +x ./Installer_Bibata.sh
-./Installer_Bibata.sh

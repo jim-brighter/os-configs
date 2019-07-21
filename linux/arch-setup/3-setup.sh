da1e663c7aaba00915ab8b3226848f418d64de6c
Jim Brighter <jim-brighter@users.noreply.github.com>
2019-07-21 00:55:29 -0400

    Create 3-setup.sh

---
+++ linux/arch-setup/3-setup.sh
@@ -0,0 +1,17 @@
+cd ~
+curl -LO https://github.com/vinceliuice/matcha/archive/master.zip
+unzip -q master.zip
+rm master.zip
+cd matcha-master
+chmod +x Install
+./Install
+
+cd ~
+curl -LO https://github.com/KaizIqbal/Bibata_Cursor/archive/master.zip
+unzip -q master.zip
+rm master.zip
+cd Bibata_Cursor-master
+chmod +x build.sh
+./build.sh
+chmod +x ./Installer_Bibata.sh
+./Installer_Bibata.sh

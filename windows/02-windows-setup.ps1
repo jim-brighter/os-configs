6ee6ce9ce3d3cbcc8c37dd35986c0fad0961056f
Jim Brighter <jbrighter92@gmail.com>
2019-11-19 23:08:52 -0500

    remove repos that don't exist anymore

--- windows/02-windows-setup.ps1
+++ windows/02-windows-setup.ps1
@@ -21,29 +21,7 @@ git clone git@github.com:jim-brighter/jim-brighter.github.io.git
 git clone git@github.com:jim-brighter/ops-secrets.git
 git clone git@github.com:jim-brighter/os-configs.git
 
-Set-Location -Path ~/projects/infostellar
-git clone https://github.com/jbrighter/samplejava.git
-git clone https://github.com/jbrighter/samplejenkins.git
-git clone https://github.com/jbrighter/devops-infrastructure.git
-git clone https://github.com/jbrighter/devops-springboot.git
-
-Set-Location -Path ~/projects/infostellar/samplejava
-git config user.email "jim@infostellarinc.com"
-
-Set-Location -Path ~/projects/infostellar/samplejenkins
-git config user.email "jim@infostellarinc.com"
-
-Set-Location -Path ~/projects/infostellar/devops-springboot
-git config user.email "jim@infostellarinc.com"
-
-Set-Location -Path ~/projects/infostellar/devops-infrastructure
-git config user.email "jim@infostellarinc.com"
-
-Set-Location -Path ~/projects/misc/shyam
-git clone git@github.com:shyamuserid/KISS-webapp.git
-git clone git@github.com:shyamuserid/cicd-wand.git
-
-Set-Location -Path ~/projects/misc/maximus
+Set-Location -Path ~/projects/maximus
 git clone git@github.com:jim-brighter/maximus-infrastructure.git
 git clone git@github.com:jim-brighter/maximus-sampleapp.git
 git clone git@github.com:jim-brighter/maximus-demo.git

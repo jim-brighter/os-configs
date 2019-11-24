2fcfe2d423b28a93bbf063baa892a1c5fb2cc806
Jim Brighter <jbrighter92@gmail.com>
2019-11-24 17:18:12 -0500

    latte dock, snap, slack, spotify

--- linux/manjaro-setup/1-init.sh
+++ linux/manjaro-setup/1-init.sh
@@ -3,7 +3,13 @@ set -ex
 pacman -S firefox gradle eclipse-jee emacs-nox \
 	docker docker-compose dbeaver git nodejs npm openssh \
 	libxss gconf code ttf-hack neofetch gtop zip unzip vim \
-	appmenu-gtk-module libdbusmenu-glib
+	appmenu-gtk-module libdbusmenu-glib latte-dock snapd
+
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+
+snap install slack --classic
+snap install spotify
 
 systemctl enable docker
 

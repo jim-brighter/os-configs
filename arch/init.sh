9c333b073225e42efd14df1f64f9415a510e4faf
Jim Brighter <jbrighter92@gmail.com>
2020-08-11 00:25:56 -0400

    move nvm init to zshrc backup

--- arch/init.sh
+++ arch/init.sh
@@ -92,8 +92,6 @@ sudo gpasswd -a jim docker
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
-
 chsh -s /usr/bin/zsh
 
 echo "if everything looks good, reboot now"

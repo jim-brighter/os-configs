95aad0e2014d4a51a45c233b1e231cec6cb5fbb0
Jim Brighter <jbrighter92@gmail.com>
2020-09-06 01:02:54 -0400

    git clone script

---
+++ git-clone.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl --location --request GET 'https://api.github.com/user/repos' \
+--header 'Accept: application/vnd.github.v3+json' \
+--header "Authorization: token $TOKEN" \
+| jq -r '.[] | [.owner.login, .ssh_url] | @tsv' \
+| while IFS=$'\t' read -r owner url; do
+    mkdir -p $owner && cd $owner && git clone $url && cd ..
+done

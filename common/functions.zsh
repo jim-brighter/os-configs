156fe9f36ae8efebbd0944b671ae37e1f76ce220
Jim Brighter <jbrighter92@gmail.com>
2025-06-19 22:02:16 -0400

    zsh functions enhancements

--- common/functions.zsh
+++ common/functions.zsh
@@ -3,21 +3,9 @@ function git-clean-branches() {
     git branch -vv | grep ': gone' | awk '{print $1}' | xargs git branch -D
 }
 
-function clone() {
-    repo=$(echo $1 | cut -d':' -f2 | sed -e 's/\.git$//')
-
-    mkdir -p ~/github
-
-    cd ~/github
-
-    gh repo clone $repo $repo
-}
-
 function clone-all() {
 
     gh auth status
-
-    ssh-add -l
 
     read "continue?Continue (Y/n) "
 
@@ -39,18 +27,17 @@ function clone-all() {
 }
 
 function start-docker() {
-    if [[ "$OSTYPE" =~ ^darwin.* ]]; then
-        open /Applications/Docker.app
-    else
+    if [[ "$OSTYPE" = "linux-gnu" ]]; then
         /mnt/c/Program\ Files/Docker/Docker/Docker\ Desktop.exe
+    else
+        open /Applications/Docker.app
     fi
 }
 
 function update() {
-    if [[ "$OSTYPE" =~ ^darwin.* ]]; then
-        brew update && brew upgrade; brew autoremove && brew cleanup
-    else
+    if [[ "$OSTYPE" = "linux-gnu" ]]; then
         sudo apt update && sudo apt full-upgrade -y; sudo apt autoremove -y
     fi
-}
 
+    brew update && brew upgrade; brew autoremove && brew cleanup
+}
